85748e4dcde4b4970862460498d5ada97049c88f
lmm <myliming@gmail.com>
2024-01-17 23:25:47 +0800

    不知道改了什么。

---
+++ src/editor.tsx
@@ -0,0 +1,17 @@
+// Import React dependencies.
+// import React, { useState } from 'react';
+import React from 'react';
+// Import the Slate editor factory.
+// import { createEditor } from 'slate';
+
+// Import the Slate components and React plugin.
+// import { Slate, Editable, withReact } from 'slate-react';
+
+const Editor = () => {
+  // Create a Slate editor object that won't change across renders.
+  //   const [editor] = useState(() => withReact(createEditor()));
+
+  return <p>Editor</p>;
+};
+
+export default Editor;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-
+import Editor from './editor';
 // Delete me
 export const Thing = () => {
   return <div>the snozzberries taste like snozzberries</div>;
 };
+
+export { Editor };
